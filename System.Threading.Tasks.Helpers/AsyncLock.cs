4a06053cd08bf471264b05e18884c1b1b0664e7c
Artyom Sokolov <artyom@applicazza.com>
2019-02-10 06:24:54 +0200

    Initial commit

---
+++ System.Threading.Tasks.Helpers/AsyncLock.cs
@@ -0,0 +1,18 @@
+namespace System.Threading.Tasks.Helpers
+{
+    public class AsyncLock
+    {
+        private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
+ 
+        public async Task<AsyncLock> LockAsync(CancellationToken token = default(CancellationToken))
+        {
+            await _semaphoreSlim.WaitAsync(token);
+            return this;
+        }
+ 
+        public void Dispose()
+        {
+            _semaphoreSlim.Release();
+        }
+    }
+}
---
+++ System.Threading.Tasks.Helpers/CancellationTokenHelper.cs
@@ -0,0 +1,12 @@
+namespace System.Threading.Tasks.Helpers
+{
+    public static class CancellationTokenHelper
+    {
+        public static Task WhenCanceled(this CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>) s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+    }
+}
